687075a7dd1e83b2c9a744afb748c802d259a7ca
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2024-11-26 19:41:11 +0200

    added Handler for interactions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,35 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-
-    private bool isWalking;
-    private Vector3 lastInteractDir;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
+
+    private bool isWalking;
+    private Vector3 lastInteractDir;
+    
+
+    private void Start(){
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e){
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 movVec = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (movVec != Vector3.zero){
+            lastInteractDir = movVec;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                clearCounter.Interact();
+            }
+        } else {
+            Debug.Log("-");
+        }
+    }
 
     private void Update()
     {
@@ -34,10 +57,10 @@ public class Player : MonoBehaviour
         float interactDistance = 2f;
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                clearCounter.Interact();
+                
             }
         } else {
-            Debug.Log("-");
+            
         }
         
 
